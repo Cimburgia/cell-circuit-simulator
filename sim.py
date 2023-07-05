5ccff90db8e6120b8dcc5e316730ffecf92f133f
Carina Imburgia <imburgia@cs.washington.edu>
2023-07-04 18:36:46 -0700

    added wire cell

--- sim.py
+++ sim.py
@@ -50,14 +50,18 @@ class Receiver(Cell):
             
 
 class Wire(Cell):
-    def __init__(self):
+    def __init__(self, signal_in):
         super().__init__()
-        self.inputs = {}
-        self.outputs = {}
+        self.inputs = {signal_in:False}
+        self.outputs = {signal_in:False}
+        self.signal_in = signal_in
+        self.signal_out = signal_in
 
-    def logic(self, signal):
-        pass
-
+    def logic(self, recv_signals):
+        for rs in recv_signals:
+            self.inputs[rs] = True
+        self.outputs[self.signal_out] = self.inputs[self.signal_in]
+    
 
 class Circuit:
     def __init__(self, c):
