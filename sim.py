fde82e17cc3c7e9affaa8fefedb1a54835077654
Carina Imburgia <imburgia@cs.washington.edu>
2023-07-17 14:39:08 -0700

    adding comments

--- sim.py
+++ sim.py
@@ -7,13 +7,22 @@ class Cell:
         self.outputs = {}
         self.from_cells = []
         self.to_cells = []
+
     
     def update(self, signals_in):
+        """
+        Calls the logic operation on itself, which processes the 
+        current inputs into outputs. Then looks at all its neighboring cells
+        and calls update on them.
+        """
         self.logic(signals_in)
         for tc in self.to_cells:
             tc.update(self.outputs)
 
     def connect(self, cell):
+        """
+        Connects itself to another cell
+        """
         self.to_cells.append(cell)
         cell.from_cells.append(self)
     
